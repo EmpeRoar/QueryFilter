2a1e1b36cc914a084560319fcae68357b67551c5
Julius Bacosa <julius@innoventsoftware.com.au>
2019-07-23 08:14:30 +0800

    Cool Query Filter Start

---
+++ jbx.queryfilter/QueryFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jbx.queryfilter
+{
+    public class QueryFilter
+    {
+        public string page { get; set; }
+        public string pageSize { get; set; }
+        public string sort { get; set; }
+        public string query { get; set; }
+    }
+}
